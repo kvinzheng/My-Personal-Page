75bacb4284fea7d44790edd5e45102e703362938
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-07-10 08:43:01 -0700

    updated

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,12 +1,8 @@
-module.exports = {
-	'extends': 'airbnb',
-	'plugins': [
-		'react',
-		'jsx-a11y',
-		'import',
-	],
-	'rules': {
-		'indent': [2, 'tab'],
-		'no-tabs': 0,
-	},
-};
+// module.exports = {
+// 	'extends': 'airbnb',
+// 	'plugins': [
+// 		'react',
+// 		'jsx-a11y',
+// 		'import',
+// 	],
+// };
--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NavBar from './components/navbar/navbar';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import navbarInstance from './components/navbar/navbar';
+import Home from './components/home/home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <div>
-        hi
+        <div>
+          hi
+        </div>
+        <Router>
+          <div className="App">
+            {/* <Route path="/" component={App}>  */}
+            <Route path='/' component={navbarInstance} />
+            <Route exact path="/" component={Home} />
+          </div>
+        </Router>
       </div>
-    <Router>
-      <div className="App">
-        <Route path='/' Component={NavBar}  />
-
-      </div>
-    </Router>
-    </div>
     );
   }
 }
