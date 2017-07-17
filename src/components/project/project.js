2e7aa8385d5550241496ae4d44670fecf3b19de2
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-07-17 15:50:48 -0700

    update node.js version

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -4,9 +4,13 @@ import Project2 from './project2';
 import Project3 from './project3';
 
 class Project extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { display1: 'show' , display2: 'none', display3: 'none'};
+    this.state = {
+      display1: 'show',
+      display2: 'none',
+      display3: 'none'
+    };
     this.toggleDisplay1 = this.toggleDisplay1.bind(this);
     this.toggleDisplay2 = this.toggleDisplay2.bind(this);
     this.toggleDisplay3 = this.toggleDisplay3.bind(this);
@@ -14,13 +18,13 @@ class Project extends Component {
   // componentDidMount(){
   //   this.state = { display1: 'none' , display2: 'none', display: 'none'};
   // }
-  toggleDisplay1(){
+  toggleDisplay1() {
     this.setState({display1: 'show', display2: 'none', display3: 'none'})
   }
-  toggleDisplay2(){
+  toggleDisplay2() {
     this.setState({display1: 'none', display2: 'show', display3: 'none'})
   }
-  toggleDisplay3(){
+  toggleDisplay3() {
     this.setState({display1: 'none', display2: 'none', display3: 'show'})
   }
 
@@ -41,8 +45,20 @@ class Project extends Component {
           <Project2 display={this.state.display2}/>
           <Project3 display={this.state.display3}/>
         </div>
-        <div>
-          <div>Buttom</div>
+        <div className="bot">
+          <div className="contact-area">
+            <h2>CONTACT <span>KEVIN ZHENG</span></h2>
+            <p className="email">kvinzheng@gmail.com</p>
+            <div className="row">
+              <a href="https://github.com/kvinzheng" target="_blank" className="icon">
+                <i id="github" className="fa fa-github fa-3x"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/kevin-zheng-ucd" target="_blank" className="icon">
+                <i id="linkedin" className="fa fa-linkedin fa-3x" ></i>
+              </a>
+            </div>
+
+          </div>
         </div>
       </div>
     )
