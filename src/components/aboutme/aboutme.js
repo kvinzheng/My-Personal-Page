9e0adc1f68bcc8dd90383d699369be846515150f
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-07-27 17:07:02 -0700

    fixed description

--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -12,7 +12,7 @@ const Aboutme = () => {
               <h3>About Me</h3>
             </div>
             <div className="summary">
-            <p >Ever since I took my first programing class from UC Davis, I have been passionate about using technology to solve problems.Prior to attending  <a href="https://www.galvanize.com/san-francisco/web-development" target="_blank" className="linkStyle">Galvanize's web development immsersive program</a>, I have sought to combine my interest in problem solving and analystical skills in positions in data analytics and business analysis.</p>
+            <p >Ever since I took my first programming class from UC Davis, I have been passionate about using technology to solve problems.Prior to attending  <a href="https://www.galvanize.com/san-francisco/web-development" target="_blank" className="linkStyle">Galvanize's web development immsersive program</a>, I have sought to combine my interest in problem solving and analystical skills in positions in data analytics and business analysis.</p>
           <br/>
             <p>My background in data analytics stems from my passion for using data to help people solve problems. Since the day I was exposed to SQL, I’ve really enjoyed the programming side in data analytics and I hope to use my engineering mindset to help companies solve problems.</p>
             </div>
